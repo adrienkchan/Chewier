8d9fe280b609c5be46e1c6ea2479f8f0baaee2ea
Lauren Lowe <lauren.lowe84@gmail.com>
2023-01-25 12:21:04 -0800

    Corrected linting test fails

--- inventory/tests/lauren_test.py
+++ inventory/tests/lauren_test.py
@@ -3,19 +3,22 @@ from main import app
 from queries.inventory import FoodBrandOut, FoodBrandRepository
 
 
-food_brand_out = FoodBrandOut(
-            id = 17,
-            name = "test",
-            animal_type = "cat"
+food_brand_out= FoodBrandOut(
+            id= 17,
+            name="test",
+            animal_type="cat"
     )
 
-client=TestClient(app)
+client= TestClient(app)
+
+
 class FakeBrandRepository:
     def get_one(self, id):
         return food_brand_out
 
+
 def test_get_brand():
     app.dependency_overrides[FoodBrandRepository] = FakeBrandRepository
-    response = client.get("/brands/17")
+    response= client.get("/brands/17")
     assert response.status_code == 200
     assert response.json() == food_brand_out
